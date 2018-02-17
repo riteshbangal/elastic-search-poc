afa762638575a1e19d7815268b6630aa4897ea31
Ritesh Bangal <riteshbangal@gmail.com>
2018-02-17 21:29:25 +0530

    Added spring data elastic search

---
+++ spring-data-elastic-search/src/test/java/com/spring/data/elastic/search/springdataelasticsearch/SpringDataElasticSearchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.data.elastic.search.springdataelasticsearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDataElasticSearchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
